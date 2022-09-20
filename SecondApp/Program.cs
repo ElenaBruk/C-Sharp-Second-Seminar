7984aaa4a1a50a9956a7f699f578bcec6d3f3ff1
Elena Bruk <aen_law@mail.ru>
2022-09-20 23:42:50 +0300

    Second App

---
+++ SecondApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int CreateNumber()
+{
+    return new Random().Next(100, 1000);
+}
+int NewDigit(int number)
+{
+    int Sot = number / 100;
+    int Ed = number % 10;
+    return Sot * 10 + Ed;
+}
+
+int a = CreateNumber();
+Console.WriteLine(a);
+int b = NewDigit(a);
+Console.WriteLine(b);
