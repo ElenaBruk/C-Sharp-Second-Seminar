ce7dd00d84d6966e65dcdd08fcd416c532d36d58
Elena Bruk <aen_law@mail.ru>
2022-09-20 23:43:49 +0300

    Third App

---
+++ ThirdApp/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+void Module(int a, int b)
+{
+    if (b % a == 0)
+    {
+        Console.WriteLine("Второе число кратно первому");
+    }
+    else
+    {
+        Console.WriteLine("Второе число не кратно первому");
+        Console.WriteLine("Остаток от деления равен " + (b % a));
+    }
+}
+Module(a, b);
